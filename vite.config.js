14575667b9350d82fa3a5808b4e60f6ddffaf885
nguyen.hailong <nguyenhailong2408@gmail.com>
2025-02-07 20:18:56 +0700

    test1

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base:'./',
-  resolve: {
-    alias: [{ find: '~', replacement: '/src' }]
-  }
+  base:'./'
+  // resolve: {
+  //   alias: [{ find: '~', replacement: '/src' }]
+  // }
 })
